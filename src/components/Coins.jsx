b8f120689f485d9b5ee75b6129b081e04ec7901f
cherrydub <cherrydub@gmail.com>
2023-06-22 18:00:40 +0100

    perfect colors for nav

--- src/components/Coins.jsx
+++ src/components/Coins.jsx
@@ -6,27 +6,32 @@ import { Coin } from "../routes/Coin";
 
 export const Coins = ({ coins, setDisplayAmount }) => {
   return (
-    <div className="container">
-      <button className="m-x-2" onClick={() => setDisplayAmount(10)}>
-        10
-      </button>
-      <button onClick={() => setDisplayAmount(25)}>25</button>
-      <button onClick={() => setDisplayAmount(100)}>100</button>
-      <div className="heading">
-        <p>#</p>
-        <p className="coin-name">Coin</p>
-        <p>Price</p>
-        <p>24hr</p>
-        <p className="hide-mobile">Volume</p>
-        <p className="hide-mobile">Mkt Cap</p>
+    <>
+      <div className="text-right px-4">
+        <button className="" onClick={() => setDisplayAmount(10)}>
+          10
+        </button>
+        <button onClick={() => setDisplayAmount(25)}>25</button>
+        <button onClick={() => setDisplayAmount(100)}>100</button>
       </div>
-      {coins.map((coin) => {
-        return (
-          <Link to={`/coin/${coin.id}`} element={<Coin />} key={coin.id}>
-            <CoinItem coin={coin} />
-          </Link>
-        );
-      })}
-    </div>
+
+      <div className="container">
+        <div className="heading">
+          <p>#</p>
+          <p className="coin-name">Coin</p>
+          <p>Price</p>
+          <p>24hr</p>
+          <p className="hide-mobile">Volume</p>
+          <p className="hide-mobile">Mkt Cap</p>
+        </div>
+        {coins.map((coin) => {
+          return (
+            <Link to={`/coin/${coin.id}`} element={<Coin />} key={coin.id}>
+              <CoinItem coin={coin} />
+            </Link>
+          );
+        })}
+      </div>
+    </>
   );
 };
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,14 +5,12 @@ import { Link } from "react-router-dom";
 export const NavBar = () => {
   return (
     <Link to="/">
-      <div className="navbar flex text-center justify-center">
+      <div className="navbar flex text-center justify-center pt-3">
         <i
-          className="fas fa-brands fa-bitcoin"
-          style={{ color: "var(--text-primary)" }}
+          className="fas fa-brands fa-bitcoin btc-logo"
+          style={{ color: "var(--text-secondary)" }}
         ></i>
-        <h1>
-          Cherry <span className="">Coins</span>
-        </h1>
+        itcoin will set you free
       </div>
     </Link>
   );
