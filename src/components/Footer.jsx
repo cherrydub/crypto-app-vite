2e303b1fb2967c60dcdcc043575b3cbb3850ac13
cherrydub <cherrydub@gmail.com>
2023-06-22 17:47:19 +0100

    added 100vh to .app so theres no whitespace

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Footer = () => {
   return (
-    <div className="text-center">
+    <div className="text-center bottom-0">
       <a href="#top">Scroll to Top</a>
     </div>
   );
